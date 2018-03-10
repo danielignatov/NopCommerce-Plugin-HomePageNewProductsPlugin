d18299ecb2ac8de27be01c2681e1805dfcd7edd2
Daniel Ignatov <daniel.m.ignatov@abv.bg>
2018-03-10 13:42:11 +0200

    Updated PublicInfo Controller Method
    Using _ProductBox partial view.
    Old Product model removed.

--- Nop.Plugin.Widgets.HomePageNewProductsPlugin/Controllers/HomePageNewProductsPluginController.cs
+++ Nop.Plugin.Widgets.HomePageNewProductsPlugin/Controllers/HomePageNewProductsPluginController.cs
@@ -8,29 +8,76 @@ using Nop.Web.Framework.Controllers;
 using Nop.Plugin.Widgets.HomePageNewProductsPlugin.Models;
 using Nop.Services.Catalog;
 using Nop.Core.Domain.Catalog;
-using Nop.Services.Seo;
+using Nop.Web.Models.Catalog;
+using System.Collections.Generic;
+using Nop.Web.Extensions;
+using Nop.Services.Security;
+using Nop.Services.Tax;
+using Nop.Services.Directory;
+using Nop.Services.Media;
+using Nop.Core.Caching;
+using Nop.Core.Domain.Media;
 
 namespace Nop.Plugin.Widgets.HomePageNewProductsPlugin.Controllers
 {
     public class HomePageNewProductsPluginController : BasePluginController
     {
+        private readonly ITaxService _taxService;
+        private readonly ICurrencyService _currencyService;
+        private readonly IPictureService _pictureService;
+        private readonly ICategoryService _categoryService;
         private readonly IWorkContext _workContext;
         private readonly IProductService _productService;
+        private readonly IPermissionService _permissionService;
         private readonly IStoreService _storeService;
+        private readonly IStoreContext _storeContext;
         private readonly ISettingService _settingService;
+        private readonly IPriceCalculationService _priceCalculationService;
+        private readonly IPriceFormatter _priceFormatter;
+        private readonly IWebHelper _webHelper;
+        private readonly ISpecificationAttributeService _specificationAttributeService;
         private readonly ILocalizationService _localizationService;
+        private readonly MediaSettings _mediaSettings;
+        private readonly CatalogSettings _catalogSettings;
+        private readonly ICacheManager _cacheManager;
 
-        public HomePageNewProductsPluginController(IWorkContext workContext,
+        public HomePageNewProductsPluginController(ITaxService taxService,
+            ICurrencyService currencyService,
+            IPictureService pictureService,
+            ICategoryService categoryService,
+            IWorkContext workContext,
             IProductService productService,
+            IPermissionService permissionService,
             IStoreService storeService,
+            IStoreContext storeContext,
             ISettingService settingService,
-            ILocalizationService localizationService)
+            IPriceCalculationService priceCalculationService,
+            IPriceFormatter priceFormatter,
+            IWebHelper webHelper,
+            ISpecificationAttributeService specificationAttributeService,
+            ILocalizationService localizationService,
+            MediaSettings mediaSettings,
+            CatalogSettings catalogSettings,
+            ICacheManager cacheManager)
         {
+            this._taxService = taxService;
+            this._currencyService = currencyService;
+            this._pictureService = pictureService;
+            this._categoryService = categoryService;
             this._workContext = workContext;
             this._productService = productService;
+            this._permissionService = permissionService;
             this._storeService = storeService;
+            this._storeContext = storeContext;
             this._settingService = settingService;
+            this._priceCalculationService = priceCalculationService;
+            this._priceFormatter = priceFormatter;
+            this._webHelper = webHelper;
+            this._specificationAttributeService = specificationAttributeService;
             this._localizationService = localizationService;
+            this._mediaSettings = mediaSettings;
+            this._catalogSettings = catalogSettings;
+            this._cacheManager = cacheManager;
         }
 
         [AdminAuthorize]
@@ -87,26 +134,27 @@ namespace Nop.Plugin.Widgets.HomePageNewProductsPlugin.Controllers
         }
 
         [ChildActionOnly]
-        public ActionResult PublicInfo(string widgetZone, object additionalData = null)
+        public ActionResult PublicInfo(int? productThumbPictureSize, string widgetZone, object additionalData = null)
         {
             var numberOfProducts = this._settingService.GetSettingByKey<int>("HomePageNewProductsPluginSettings.NumberOfProductsToDisplay");
-            var products = _productService.SearchProducts(orderBy: ProductSortingEnum.CreatedOn).Take(numberOfProducts);
-            var productsModel = products
-              .Select(x =>
-              {
-                  var latestModel = new LatestProductModel
-                  {
-                      Id = x.Id,
-                      Name = x.Name,
-                      Picture = x.ProductPictures.FirstOrDefault(),
-                      SeName = x.GetSeName()
-                  };
-                  return latestModel;
-              })
-              .Where(i => i.Picture != null)
-              .ToList();
+            var products = _productService.SearchProducts(orderBy: ProductSortingEnum.CreatedOn).Where(i => i.ProductPictures.FirstOrDefault() != null).Take(numberOfProducts);
+            
+            bool preparePriceModel = true;
+            bool preparePictureModel = true;
+            bool prepareSpecificationAttributes = false;
+            bool forceRedirectionAfterAddingToCart = false;
 
-            return View("~/Plugins/Widgets.HomePageNewProductsPlugin/Views/HomePageNewProductsPlugin/PublicInfo.cshtml", productsModel);
+            IList<ProductOverviewModel> productOverviewModelList = this.PrepareProductOverviewModels(_workContext,
+                _storeContext, _categoryService, _productService, _specificationAttributeService,
+                _priceCalculationService, _priceFormatter, _permissionService,
+                _localizationService, _taxService, _currencyService,
+                _pictureService, _webHelper, _cacheManager,
+                _catalogSettings, _mediaSettings, products,
+                preparePriceModel, preparePictureModel,
+                productThumbPictureSize, prepareSpecificationAttributes,
+                forceRedirectionAfterAddingToCart).ToList();
+
+            return View("~/Plugins/Widgets.HomePageNewProductsPlugin/Views/HomePageNewProductsPlugin/PublicInfo.cshtml", productOverviewModelList);
         }
     }
 }
